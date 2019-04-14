ce6528a6f0a9b54032955e5b5ca72a715ea3b509
geparada <geparada88@gmail.com>
2019-04-14 03:59:46 +0100

    Update get_U2_score.py

--- get_U2_score.py
+++ get_U2_score.py
@@ -110,7 +110,7 @@ def main(exon_bed, GT_AG_U2_5, GT_AG_U2_3):
 		
 		chrom, start, end, name, cero, strand = row
 		
-		estart, eend = map(int, name.split("_"))
+		estart, eend = map(int, name.split("_")[-2:])
 		
 		U2_E5, U2_E3 = U2Score(chrom, strand, estart, eend, U2_GTAG_3, U2_GTAG_5, U2_GTAG_3_max_score, U2_GTAG_5_max_score)
 
