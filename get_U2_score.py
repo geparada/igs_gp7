0e4c52c4ae20a4f7ca986189536b4ef51a83a943
geparada <geparada88@gmail.com>
2019-04-14 03:36:33 +0100

    Update get_U2_score.py

--- get_U2_score.py
+++ get_U2_score.py
@@ -99,6 +99,8 @@ def U2Score(ME_chr, ME_strand, ME_start, ME_end, U2_GTAG_3, U2_GTAG_5,  max_U2_s
   
 def main(exon_bed, U2_GTAG_5_file, U2_GTAG_3_file):
 	
+	print U2_GTAG_5_file
+	
 	U2_GTAG_5 = PWM_to_dict(U2_GTAG_5_file)
 	U2_GTAG_3 = PWM_to_dict(U2_GTAG_3_file)	
 	
