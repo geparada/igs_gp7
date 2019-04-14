b48aa8a9072a4fe35b9b4997497bfaf6d4a26241
geparada <geparada88@gmail.com>
2019-04-14 02:51:42 +0100

    Update get_U2_score.py

--- get_U2_score.py
+++ get_U2_score.py
@@ -1,5 +1,5 @@
-
-
+import sys
+import csv
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.Alphabet import generic_dna
@@ -60,11 +60,80 @@ def PWM_to_dict(file):
 	matrix["N"] = N_freq
 	
 	return matrix
+
+
+def U2Score(ME_chr, ME_strand, ME_start, ME_end, U2_GTAG_3, U2_GTAG_5,  max_U2_score_3, max_U2_score_5):
+
+    ME5 = str(Genome[ME_chr][ME_start-14:ME_start+3]).upper()
+    ME3 = str(Genome[ME_chr][ME_end-3:ME_end+10]).upper()
+
+
+    if ME_strand == "-":
+
+        ME5 = str(Genome[ME_chr][ME_end-3:ME_end+14].reverse_complement()).upper()
+        ME3 = str(Genome[ME_chr][ME_start-10:ME_start+3].reverse_complement()).upper()
+
+
+
+    U2_ME5 = 0
+    U2_ME3 = 0
+
+    i = 0
+
+    for N in ME5:
+        U2_ME5 += U2_GTAG_3[N][i]
+        i += 1
+
+    i = 0
+
+    for N in ME3:
+        U2_ME3 += U2_GTAG_5[N][i]
+        i += 1
+
+    U2_ME5 = percent(U2_ME5, max_U2_score_3)
+    U2_ME3 = percent(U2_ME3, max_U2_score_5)
+
+    return(U2_ME5, U2_ME3)
+
+
   
-  
+def main(exon_bed, U2_GTAG_5, U2_GTAG_3, GT_AG_U2_5, GT_AG_U2_3):
+	
+	U2_GTAG_5 = PWM_to_dict(U2_GTAG_5_file)
+	U2_GTAG_3 = PWM_to_dict(U2_GTAG_3_file)	
+	
+	with open(GT_AG_U2_5) as U2_GTAG_5_file, open(GT_AG_U2_3) as U2_GTAG_3_file:
+
+
+		U2_GTAG_5 = PWM_to_dict(U2_GTAG_5_file)
+		U2_GTAG_3 = PWM_to_dict(U2_GTAG_3_file)
+
+		U2_GTAG_5_max_score = 0
+		U2_GTAG_3_max_score = 0
+
+		for index in range(13):
+			U2_GTAG_5_max_score += max(U2_GTAG_5['A'][index], U2_GTAG_5['C'][index], U2_GTAG_5['T'][index], U2_GTAG_5['G'][index])
+
+		for index in range(17):
+			U2_GTAG_3_max_score += max(U2_GTAG_3['A'][index], U2_GTAG_3['C'][index], U2_GTAG_3['T'][index], U2_GTAG_3['G'][index])
+
+	
+	for row in csv.reader(open(exon_bed), delimiter="\t"):
+		
+		chrom, start, end, name, cero, strand = row
+		
+		start = int(start)
+		end = int(end)
+		
+		U2 = U2Score(chrom, strand, start, end, U2_GTAG_3, U2_GTAG_5, U2_GTAG_3_max_score, U2_GTAG_5_max_score)
+		
+		print(name, U2)
+
+	
+	
   
   if __name__ == '__main__':
 	Genomictabulator(sys.argv[1])
-	main(sys.argv[4])
+	main(sys.argv[2], sys.argv[3], sys.argv[4])
   
   
