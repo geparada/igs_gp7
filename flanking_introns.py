d38f2110b6d6e3b22470986c2d230042143fae31
geparada <geparada88@gmail.com>
2019-04-17 02:04:32 +0100

    Update flanking_introns.py

--- flanking_introns.py
+++ flanking_introns.py
@@ -1,38 +1,56 @@
 import sys
 import csv
 
+##  q1 + b1 ] ------[q2     q2+b2] ----- [q3 
 
-def main(bed12, ME_len):
+def main(bed12, VASTDB_PSI):
 
-	n = 100
-
-	transcript_intron_info = defaultdict(list)
-
-	min_intron_lenght = 80
-
+	exon_introns = dict()
+	
 	for row in csv.reader(open(bed12), delimiter = '\t'):
 
-		try:
 
+		qstarts = list(map(int, row[11].strip(",").split(",")))
+		blocksizes = list(map(int, row[10].strip(",").split(",")))
 
-			qName = row[3]
-			seq = Transcriptome[qName]
+		start = int(row[1])
+		strand = row[5]
+		bn = int(row[9])
+		chrom = row[0]
+		qstart = 0
 
-			qstarts = map (int, row[11].strip(",").split(","))
-			blocksizes = map(int, row[10].strip(",").split(","))
+		for q1, q2, q3, b1, b2 in zip(qstarts, qstarts[1:], qstarts[2:], blocksizes, blocksizes[1:]):
 
-			start = int(row[1])
-			strand = row[5]
-			bn = int(row[9])
-			chr = row[0]
-			qstart = 0
+			istart_up = start + q1 + b1
+			iend_up = start + q2
 
-			for q1, q2, b, b2 in zip(qstarts, qstarts[1:], blocksizes, blocksizes[1:]):
+			estart = start + q2
+			eend = start + q2 + b2
 			
-      
-      
+			istart_down = start + q2 + b2
+			iend_down = start + q3
+			
+			exon = (chrom, strand, estart, eend)
+			intron_up = (chrom, starnd, istart_up, iend_up)
+			intron_down = (chrom, starnd, istart_down, iend_down)
+			
+			exon_introns[exon] = (intron_up, intron_down)
+			
+	for row in csv.reader(open(VASTDB_PSI), delimiter = '\t'):
+		
+		exon_coords = row[1]
+		chrom = exon_coords.split(":")[0]
+		estart, eend =  exon_coords.split(":")[1].split("-")
+		estart = int(estart)
+		eend = int(eend)
+		
+		if exon in exon_introns:
+			intron_up, intron_down = exon_introns[exon]
+			intron_up_len = intron_up[-1] - intron_up[-2]
+			intron_down_len = intron_down[-1] - intron_down[-2]
+			
+			print( row, str(intron_up_len), str(intron_down_len), sep="\t" )
+		
 
 if __name__ == '__main__':
-	Genomictabulator(sys.argv[1])
-	Transcriptometabulator(sys.argv[2])
-	main (sys.argv[3], int(sys.argv[4]))
+	main (sys.argv[1], int(sys.argv[2])
